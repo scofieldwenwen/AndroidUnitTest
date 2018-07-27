de7de3d9a3d2eea62142ff097bc4360baec201f8
scofieldwenwen <scofieldwenwen@163.com>
2018-07-27 11:43:29 +0800

    1.Initial commit 2.Use Jvm to test java code

---
+++ app/src/main/java/com/scofieldwenewn/androidunittest/Calculator.java
@@ -0,0 +1,23 @@
+package com.scofieldwenewn.androidunittest;
+
+/**
+ * @author scofield@tronsis.com
+ * @date 2018/7/27 11:25
+ */
+public class Calculator {
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+}
---
+++ app/src/test/java/com/scofieldwenewn/androidunittest/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.scofieldwenewn.androidunittest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author scofield@tronsis.com
+ * @date 2018/7/27 11:30
+ */
+public class CalculatorTest {
+    private Calculator mCalculator;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mCalculator = new Calculator();
+    }
+
+    @Test
+    public void sum() {
+        assertEquals(6d, mCalculator.sum(1d, 5d), 0);
+    }
+
+    @Test
+    public void substract() {
+        assertEquals(1d,mCalculator.substract(5d,4d),0);
+    }
+
+    @Test
+    public void divide() {
+        assertEquals(4d, mCalculator.divide(20d, 5d), 0);
+
+    }
+
+    @Test
+    public void multiply() {
+        assertEquals(10d, mCalculator.multiply(2d, 5d), 0);
+    }
+}
